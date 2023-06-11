a7838b88662db8dc2c2ef221fdb0d7d6dd44fdb5
zhouyue <zhouyue@camel4u.com>
2023-06-11 17:03:56 +0800

    presets config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,8 @@
+import { getDirname, path } from "@vuepress/utils";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: "/",
@@ -22,4 +25,10 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue"
+    ),
+  },
 });
