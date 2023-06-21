aef25007a8b5b87a5c4a7ba6cf1aeb66af5856c2
zhouyue <zhouyue@camel4u.com>
2023-06-21 11:58:38 +0800

    side bar and title fix

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,22 +1,43 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const enSidebar = sidebar({
-  "/en/": [
-    "",
+// export const enSidebar = sidebar({
+//   "/en/": [
+//     "",
+//     {
+//       text: "Demo",
+//       icon: "laptop-code",
+//       prefix: "demo/",
+//       link: "demo/",
+//       // children: "structure",
+//       activeMatch: "^/demo",
+//     },
+//     {
+//       text: "Articles",
+//       icon: "book",
+//       prefix: "posts/",
+//       children: "structure",
+//     },
+//     "intro",
+//     "slides",
+//   ],
+// });
+
+export const enSidebar = sidebar(
+{
+  "/en/demo/": [
     {
       text: "Demo",
       icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
+      // prefix: "/",
+      link: "/en/demo/",
       children: "structure",
-    },
-    {
-      text: "Articles",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    "intro",
-    "slides",
+  },
   ],
-});
+
+  "/posts/": [
+    "" /* /bar/ */,
+    "three" /* /bar/three.html */,
+    "four" /* /bar/four.html */,
+  ],
+}
+);
--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,25 +1,36 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const zhSidebar = sidebar({
-  "time/": 
-  [
-    {
-      text: "时间管理",
-      icon: "book",
-      prefix: "time/",
-      children: "structure",
-    },
-  ],
-});
-// export const zhSidebar = sidebar(
+// export const zhSidebar = sidebar({
+//   "time/": 
 //   [
-//   {
-//     text: "指南",
-//     link: "/time/",
-//     icon: "lightbulb",
-//     children: "structure",
-//     // 仅在 `/zh/guide/` 激活
-//     activeMatch: "^/time",
-//   },
-// ]
-// );
+//     {
+//       text: "时间管理",
+//       icon: "book",
+//       prefix: "time/",
+//       children: "structure",
+//     },
+//   ],
+// });
+
+
+export const zhSidebar = sidebar(
+  {
+    "/kcp/": [
+      {
+        text: "KCP解读",
+        icon: "laptop-code",
+        link: "/kcp/",
+        children: "structure",
+      },
+    ],
+  
+    "/time/": [
+      {
+        text: "时间管理",
+        icon: "laptop-code",
+        link: "/time/",
+        children: "structure",
+      },
+    ],
+  }
+  );
