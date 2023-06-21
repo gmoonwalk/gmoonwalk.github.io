3fbac1b2b2f28efa2c9c72fa29abd5987246acb4
zhouyue <zhouyue@camel4u.com>
2023-06-21 11:17:40 +0800

    add kcps

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,8 +1,8 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/": [
-    "",
+  "time/": 
+  [
     {
       text: "时间管理",
       icon: "book",
@@ -11,3 +11,15 @@ export const zhSidebar = sidebar({
     },
   ],
 });
+// export const zhSidebar = sidebar(
+//   [
+//   {
+//     text: "指南",
+//     link: "/time/",
+//     icon: "lightbulb",
+//     children: "structure",
+//     // 仅在 `/zh/guide/` 激活
+//     activeMatch: "^/time",
+//   },
+// ]
+// );
